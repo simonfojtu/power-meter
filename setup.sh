2e687ced25b5114191d7104d4d44b2121b701ebe
Simon Fojtu <simon.fojtu@gmail.com>
2017-10-25 22:31:40 +0200

    script to replicate working environment on the RPi

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+# Replicate the setup on the Raspberry PI
+
+./install_commons.sh fpd
+
+mkdir power_meter
+cd power_meter
+
+virtualenv --python=python3.5 venv
+. venv/bin/activate
+
+pip install adafruit-mcp3008
+

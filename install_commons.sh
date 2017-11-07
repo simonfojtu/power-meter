0c73e9b321b0d1a07f4ab848c014c2755f57a332
Simon Fojtu <simon.fojtu@gmail.com>
2017-11-07 13:56:55 +0100

    Script to install common packages on a RPi.

---
+++ install_commons.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+# usage: $0 <user@host>
+#
+# Install common packages on the host machine as user
+
+TARGET=$1
+
+PACKAGES="vim git screen build-essential python3-dev virtualenv"
+
+ssh $TARGET "sudo apt-get install -y $PACKAGES"

70c87b5bda843d9dd85cf168ff4ab522a58d23f2
Simon Fojtu <simon.fojtu@gmail.com>
2017-11-09 22:27:41 +0100

    Crude measurement of real power

--- software/simpletest.py
+++ software/simpletest.py
@@ -1,49 +1,107 @@
-# Simple example of reading the MCP3008 analog input channels and printing
-# them all out.
-# Author: Tony DiCola
-# License: Public Domain
-from datetime import datetime
-import time
-
+import argparse
+import numpy as np
 # Import SPI library (for hardware SPI) and MCP3008 library.
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
 
-# Software SPI configuration:
-#CLK  = 18
-#MISO = 23
-#MOSI = 24
-#CS   = 25
-#mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+class Interface:
+    MAX_NUM_CHANNELS=8
 
-# Hardware SPI configuration:
-SPI_PORT   = 0
-SPI_DEVICE = 0
-mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+    def __init__(self, spi_port = 0, spi_device = 0, *, num_channels = MAX_NUM_CHANNELS):
+        self.mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(spi_port, spi_device))
+        self.num_channels = num_channels
+        self.calibrated_zero = np.zeros((self.num_channels,))
+        self.calibrated_gain = np.zeros((self.num_channels,))
 
 
-print('Reading MCP3008 values, press Ctrl-C to quit...')
-# Print nice channel column headers.
-print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
-print('-' * 57)
+    def read_raw(self, idx):
+        """Return analog value on channel idx"""
+        return self.mcp.read_adc(idx)
 
-last_second = -1
+
+    def read(self, idx):
+        return (self.read_raw(idx) - self.calibrated_zero[idx]) * self.calibrated_gain[idx]
+
+
+    def set_zero(self, idx, value):
+        self.calibrated_zero[idx] = value
+
+
+    def set_gain(self, idx, value):
+        self.calibrated_gain[idx] = value
+
+
+    def calibrate_zero(self, idx, count=1000):
+        """Assuming a 'zero' signal on channel idx, compute the mean over count measurements"""
+        x = 0
+        for _ in range(count):
+            x += self.read_raw(idx)
+        self.calibrated_zero[idx] = x / count
+        return self.calibrated_zero[idx]
+
+
+    def calibrate_gain(self, idx, count=1000):
+        x = np.zeros((count,))
+        for i in range(count):
+            x[i] = self.read(idx)
+        return np.mean(np.sort(x)[-count//10:])
+
+
+
+parser = argparse.ArgumentParser(description='AC power measurement')
+parser.add_argument('-n', type=int, default=8, help='Number of channels to use')
+parser.add_argument('-c', action='append', type=int, default=None, help='Return bias calibration value for given channel')
+parser.add_argument('-C', action='append', type=float, default=[], help='Zero calibration for each channel')
+parser.add_argument('-g', action='append', type=int, default=None, help='Return gain calibration value for given channel')
+parser.add_argument('-G', action='append', type=float, default=[], help='Gain calibration for each channel')
+
+args = parser.parse_args()
+
+adc = Interface(num_channels = args.n)
+
+if len(args.C) not in (0, adc.num_channels):
+    raise ValueError('Specify zero calibration either for all channels or for none')
+for i in range(len(args.C)):
+    adc.set_zero(i, args.C[i])
+if args.c is not None:
+    for channel in args.c:
+        print('Calibrating zero of channel {}'.format(channel))
+        print(adc.calibrate_zero(channel))
+    exit()
+
+
+if len(args.G) not in (0, adc.num_channels):
+    raise ValueError('Specify gain either for all channels or for none')
+for i in range(len(args.G)):
+    adc.set_gain(i, args.G[i])
+if args.g is not None:
+    for channel in args.g:
+        print('Calibrating gain of channel {}'.format(channel))
+        print(adc.calibrate_gain(channel))
+    exit()
+
+print('Starting measurements')
+
 counter = 0
-# Main program loop.
+sum_of_powers = 0
+
+num_periods = 0
+
+u = 0
+
 while True:
-    # Read all the ADC channel values in a list.
-    values = [0]*8
-    for i in range(8):
-        # The read_adc function will get the value of the specified channel (0-7).
-        values[i] = mcp.read_adc(i)
-    now = datetime.now()
-    if now.second != last_second:
-        last_second = now.second
-        print(counter)
-        counter = 0
-    else:
-        counter += 1
-    # Print the ADC values.
-#    print('{0} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} | {8:>4}'.format(str(datetime.now()), *values))
-    # Pause for half a second.
+    last_u = u
+    i = adc.read(0)
+    u = adc.read(1)
+
+    sum_of_powers += i * u
+    counter += 1
+    if last_u <= 0 and u > 0:
+        num_periods += 1
+        if num_periods == 50:
+            print('{:3.1f}W'.format(sum_of_powers / counter))
+            counter = 0
+            sum_of_powers = 0
+            num_periods = 0
+
